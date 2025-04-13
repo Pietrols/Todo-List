6935fa0534e017fd8bdfcc47d2527ceb41c1ca73
Peter Kabamba <kabambapeter3@gmail.com>
2025-04-13 12:29:40 +0200

    implemented addTodo, removeTodo and getTodoById methods in project.js

--- src/logic/Project.js
+++ src/logic/Project.js
@@ -1,7 +1,23 @@
+import Todo from "./Todo.js";
 export class Project {
   constructor(name, id = Date.now().toString(), todos = []) {
     this.name = name;
     this.id = id;
     this.todos = todos;
+  }
+
+  addTodo(todoInstance) {
+    if (todoInstance instanceof Todo) {
+      this.todos.push(todoInstance);
+      todoInstance.projectId = this.id;
+    }
+  }
+
+  removeTodo(todoId) {
+    this.todos = this.todos.filter((todo) => todo.id !== todoId);
+  }
+
+  getTodoById(todoId) {
+    return this.todos.find((todo) => todo.id === todoId);
   }
 }
